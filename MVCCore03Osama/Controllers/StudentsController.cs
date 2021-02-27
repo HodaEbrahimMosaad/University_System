29f852b90feec1e0b736f122c7cd8f3d4f46b5cc
Aliaa Yasser <aliaayasser89@gmail.com>
2021-02-27 16:38:24 +0200

    aliaa1

---
+++ MVCCore03Osama/Controllers/StudentsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCCore03Osama.Service;
+using MVCCore03Osama.Models;
+namespace MVCCore03Osama.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IStudent student;
+        public StudentsController(IStudent _student )
+        {
+            student = _student;
+        }
+        public async Task< IActionResult> allStudents()
+        {
+            return View(await student.getAllStudents());
+        }
+        [HttpGet]
+        public  IActionResult Edit(string id)
+        {
+            var _student = student.getStudent(id);
+            if (_student!=null)
+            {
+                return View(_student);
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(  Student _student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.EditStudent(_student);
+                return RedirectToAction("allStudents");
+            }
+            return View(_student);
+        }
+        public IActionResult Details(string id)
+        {
+
+                return View(student.getStudent(id));
+            
+        }
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            return View(student.getStudent(id));
+        }
+        [HttpPost]
+        public IActionResult Delete(string id ,Student _student)
+        {
+            bool isDeleted = student.deleteStudent(id, _student);
+            if (isDeleted)
+            {
+                return RedirectToAction(nameof(allStudents));
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create( Student _student)
+        {
+           bool isCreated = await student.createStudent( _student);
+            if (isCreated == true)
+            {
+                return RedirectToAction(nameof(allStudents));
+            }
+            return View( _student);
+        }
+    }
+
+}
